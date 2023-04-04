736a7f8005e7af04601c78e36cdac56f23f14a31
stonefullstm <stonefull.stm@gmail.com>
2023-04-04 15:56:09 -0300

    feat: Requisito 3: Implementação do método /generate/ na classe /CompleteReport/

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, stock_data):
+        companies = Counter(
+            product["nome_da_empresa"] for product in stock_data)
+
+        header = "Produtos estocados por empresa:"
+        details = ""
+        for company in companies:
+            details += f"- {company}: {companies[company]}\n"
+
+        return (
+            f"{super().generate(stock_data)}\n"
+            f"{header}\n"
+            f"{details}"
+        )
