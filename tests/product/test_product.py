22af9c94020da89ab31c9947e8995eb5b76fddb3
stonefullstm <stonefull.stm@gmail.com>
2023-04-04 18:25:31 -0300

    test: Requisito 8 - Implementação do teste da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,12 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(1, "Azeite", "Trás-os-Montes Azeites",
+                      "18-02-2021", "17-09-2023", "AZ25 1551 4467 2549 4402 1",
+                      "em lugar fresco e seco")
+    assert produto.__repr__() == (
+            "O produto Azeite fabricado em 18-02-2021 "
+            "por Trás-os-Montes Azeites com validade até 17-09-2023 precisa "
+            "ser armazenado em lugar fresco e seco."
+        )
