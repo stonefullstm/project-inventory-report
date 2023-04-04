d76e7e4e443c5e6580a64e9420071b846541d764
stonefullstm <stonefull.stm@gmail.com>
2023-04-04 15:33:52 -0300

    feat: Requisito 2: Implementação do método /generate/ em /SimpleReport/

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,5 +1,26 @@
-# Classe relatório simples
+from datetime import datetime
+from collections import Counter
 
 
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, stock_data):
+        oldest_manufacturing_date = min(
+            [product["data_de_fabricacao"] for product in stock_data])
+
+        nearest_expiration_date = min(
+            [product["data_de_validade"] for product in stock_data
+                if datetime.strptime(product["data_de_validade"], "%Y-%m-%d")
+                >= datetime.today()])
+
+        # source: https://acervolima.com/
+        #   python-contar-ocorrencias-de-um-elemento-em-uma-lista/
+        cias = Counter(product["nome_da_empresa"] for product in stock_data)
+        # source: https://datagy.io/python-get-dictionary-key-with-max-value/
+        cia_with_more_products = max(cias, key=cias.get)
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {nearest_expiration_date}\n"
+            f"Empresa com mais produtos: {cia_with_more_products}"
+        )
