00b6a096c2f49c707c3964adeab16fdeac7c0fb8
stonefullstm <stonefull.stm@gmail.com>
2023-04-05 15:04:41 -0300

    refactor: Teste de criação do produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -5,7 +5,7 @@ def test_cria_produto():
     produto = Product(1, "Azeite", "Trás-os-Montes Azeites",
                       "18-02-2021", "17-09-2023", "AZ25 1551 4467 2549 4402 1",
                       "em lugar fresco e seco")
-    assert produto.__repr__() == (
+    assert str(produto) == (
             "O produto Azeite fabricado em 18-02-2021 "
             "por Trás-os-Montes Azeites com validade até 17-09-2023 precisa "
             "ser armazenado em lugar fresco e seco."
