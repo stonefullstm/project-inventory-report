f6524db2f6881940de4319f79fad50d573817b7b
stonefullstm <stonefull.stm@gmail.com>
2023-04-05 18:06:10 -0300

    test: Teste da classe ColoredReport

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,23 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from tests.report_decorator.stock_data import data
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+import pytest
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def mock_data():
+    return data
+
+
+def test_decorar_relatorio(mock_data):
+    report = ColoredReport(SimpleReport)
+    result = (
+        "\033[32mData de fabricação mais antiga:\033[0m "
+        "\033[36m2020-09-06\033[0m\n"
+        "\033[32mData de validade mais próxima:\033[0m "
+        "\033[36m2023-09-17\033[0m\n"
+        "\033[32mEmpresa com mais produtos:\033[0m "
+        "\033[31mTarget Corporation\033[0m"
+    )
+
+    assert report.generate(mock_data) == result
